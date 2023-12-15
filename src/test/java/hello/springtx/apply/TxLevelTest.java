5c12b034f2f83962d60a1e5b77e2b5862b4e265d
sdongpil <pildong94@naver.com>
2023-12-16 02:05:45 +0900

    프록시 내부 호출 예제

---
+++ src/test/java/hello/springtx/apply/TxLevelTest.java
@@ -0,0 +1,56 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class TxLevelTest {
+
+    @Autowired
+    private LevelService levelService;
+
+
+    @Test
+    void t1() {
+        levelService.write();
+        levelService.read();
+    }
+
+    @TestConfiguration
+    static class TxLevelTestConfig {
+        @Bean
+        LevelService levelService() {
+            return new LevelService();
+        }
+    }
+
+    @Slf4j
+    @Transactional(readOnly = true)
+    static class LevelService {
+
+        @Transactional(readOnly = false)
+        public void write() {
+            log.info("call write");
+            printTx();
+        }
+
+        public void read() {
+            log.info("call read");
+            printTx();
+        }
+
+        public void printTx() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active ={}", txActive);
+            boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+            log.info("readOnly={}", readOnly);
+        }
+    }
+}
